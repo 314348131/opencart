17cf833978f3de4c18c5c75596e781bdc1462169
yang <yang@qq.com>
2020-09-16 19:40:33 +0800

    12

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
+//无敌
 
 Vue.config.productionTip = false;
 new Vue({
