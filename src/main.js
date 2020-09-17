2a96435e81b8b52e58438ab29158e182776ef330
hardacheng <824276041@qq.com>
2020-09-17 09:34:13 +0800

    凯凯的第二次

--- src/main.js
+++ src/main.js
@@ -5,12 +5,10 @@ import store from "./store";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
-<<<<<<< HEAD
 //凯哥无敌
 
-=======
+
 //无敌    是是是
->>>>>>> master
 
 Vue.config.productionTip = false;
 new Vue({
